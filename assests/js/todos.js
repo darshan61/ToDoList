f643625fb86b7918b647fb6a6f9114bf8c26530c
darshan61 <32760337+darshan61@users.noreply.github.com>
2018-09-04 10:10:33 +0530

    Initial commit

---
+++ assests/js/todos.js
@@ -0,0 +1,18 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+$("input").on("keypress",function (e){
+	if(e.which === 13){
+		var newItem = $(this).val();
+		$("ul").append("<li><span>X </span>"+ newItem +"</li>");
+		$(this).val("");
+	}
+});
+
+$("ul").on("click","span",function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
